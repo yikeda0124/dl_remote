31717c4e5516d6e6d239ae87b5b7417c5bb4ba68
yikeda0124 <63008530+yikeda0124@users.noreply.github.com>
2021-04-09 11:53:10 +0900

    add

---
+++ BUILD-DOCKER-CONTAINER.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=dl_remote
+docker build . -t matsuolab/dl_remote
---
+++ RUN-DOCKER-CONTAINER.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=dl_remote
+CONTAINER_NAME=yikeda_${PROJECT_NAME}
+IMAGE_NAME=matsuolab/${PROJECT_NAME}
+TAG_NAME=latest
+
+docker run -it --rm \
+    --gpus all \
+    -p 5900:5900 \
+    -p 8888:8888 \
+    -e DISPLAY=:0\
+    -v ${PWD}/work:/root/work \
+    --name ${CONTAINER_NAME} \
+    ${IMAGE_NAME}:${TAG_NAME}
